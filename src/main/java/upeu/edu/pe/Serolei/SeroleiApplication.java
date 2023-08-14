68f244dd2b99447a5dcc82933fcb984423807424
MeBS-0123 <medalith-becerril@upeu.edu.pe>
2023-08-14 12:20:55 -0500

    Iniciado projecto

---
+++ src/main/java/upeu/edu/pe/Serolei/SeroleiApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.Serolei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeroleiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeroleiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/Serolei/SeroleiApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.Serolei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeroleiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
